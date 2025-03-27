050eb4ea250cdd14bf19a81de73a32c6b76edda9
WanYongzhi <1819298649@qq.com>
2025-03-27 20:35:34 +0800

    fix:修改内容

--- week02/classtask/struct_01.go
+++ week02/classtask/struct_01.go
@@ -21,13 +21,13 @@ func NewPerson(name, email string, age int) Person {
 }
 
 func (p Person) PrintPerson() {
-	data, err := json.Marshal(p)
+	data, err := json.MarshalIndent(p, "", "   ")
 	if err != nil {
 		fmt.Println("转换出现错误！")
 		return
 	}
 	fmt.Printf("姓名：%s，年龄：%v，邮箱：%s\n", p.Name, p.Age, p.Email)
-	fmt.Printf("json格式输出:%s\n", data)
+	fmt.Printf("json格式化输出:%s", data)
 }
 
 func main() {
