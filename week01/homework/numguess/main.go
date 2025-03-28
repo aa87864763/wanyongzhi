64aee5d9f450f356ad1df8eed0cbd1b112d8da58
WanYongzhi <1819298649@qq.com>
2025-03-28 11:52:08 +0800

    fix:修改内容

--- week01/homework/numguess/main.go
+++ week01/homework/numguess/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -8,58 +10,72 @@ import (
 	"time"
 )
 
-func guess(t int, roundNumber int) (bool, []string) {
+type Game struct {
+	GameCount int     `json:"游玩轮次"`
+	StartTime string  `json:"开始时间"`
+	EndTime   string  `json:"结束时间"`
+	UseTime   string  `json:"消耗时间"`
+	Rounds    []Round `json:"轮次信息"`
+}
+
+type Round struct {
+	RoundNumber  int    `json:"轮次编号"`
+	Result       string `json:"本轮结果"`
+	RandomNumber int    `json:"生成数字"`
+	GuessNumbers []int  `json:"猜测数字"`
+	UseTime      string `json:"本轮耗时"`
+}
+
+// 进行猜测
+func guess(t int, roundNumber int) (bool, Round) {
 	var i int
 
 	randomNum := rand.Intn(100) + 1
+	fmt.Println(randomNum)
 	fmt.Println("开始游戏：\n")
-	RoundStartTime := time.Now() //轮次开始时间
-	roundData := []string{       //创建轮次数据切片，并往轮次数据切片中添加当前轮次开始数据
-		fmt.Sprintf("第%d轮游戏开始时间为：%s,生成随机数为：%d", roundNumber, RoundStartTime.Format("2006-01-02 15:04:05"), randomNum),
+	RoundStartTime := time.Now() // 轮次开始时间
+
+	round := Round{
+		RoundNumber:  roundNumber,
+		GuessNumbers: []int{},
+		RandomNumber: randomNum,
 	}
+
 	answer := "错误"
-	for x := 0; x < t; x++ { //进行猜测游戏
+	for x := 0; x < t; x++ { // 进行猜测游戏
 		fmt.Printf("第%d次猜测，请输入您的数字(1 - 100)：\n", x+1)
-		NumStartTime := time.Now() //猜测开始时间
 		fmt.Scan(&i)
-		NumEndTime := time.Now() //猜测结束时间
 		temp := i
-		useTime := NumEndTime.Sub(NumStartTime) //计算猜测所需时间
-		useTimeStr := useTime.String()          //将time.Time转换为字符串类型
+
+		round.GuessNumbers = append(round.GuessNumbers, temp)
 
 		if temp == randomNum {
 			answer = "正确"
 			fmt.Printf("恭喜您猜对了！您在第%d次猜测中成功。\n", x+1)
-			fmt.Println("耗时：", useTime)
-			roundData = append(roundData, //往轮次数据切片中添加该次猜测数据
-				fmt.Sprintf("第%d轮：第%d次猜测数据：耗时:%s; 猜测数字:%d; 开始时间：%s; 结束时间：%s;", roundNumber, x+1, useTimeStr, temp, NumStartTime.Format("2006-01-02 15:04:05"), NumEndTime.Format("2006-01-02 15:04:05")))
 			break
-		} else if temp > 100 || temp < 1 { //处理猜测数字不在范围内的情况
+		} else if temp > 100 || temp < 1 { // 处理猜测数字不在范围内的情况
 			fmt.Println("请输入(1 - 100)的数字。\n")
-			fmt.Println("耗时：", useTime)
 		} else if temp > randomNum {
 			fmt.Println("您猜的数字大了。\n")
-			fmt.Println("耗时：", useTime)
 		} else {
 			fmt.Println("您猜的数字小了。\n")
-			fmt.Println("耗时：", useTime)
 		}
-		roundData = append(roundData, //往轮次数据切片中添加该次猜测数据
-			fmt.Sprintf("第%d轮：第%d次猜测数据：耗时:%s; 猜测数字:%d; 开始时间：%s; 结束时间：%s;", roundNumber, x+1, useTimeStr, temp, NumStartTime.Format("2006-01-02 15:04:05"), NumEndTime.Format("2006-01-02 15:04:05")))
 	}
-	RoundEndTime := time.Now() //轮次结束时间
+
+	RoundEndTime := time.Now() // 轮次结束时间
 	RoundUseTime := RoundEndTime.Sub(RoundStartTime)
 	RoundUseTimeStr := RoundUseTime.String()
-	roundData = append(roundData, //往轮次数据切片中添加当前轮次结束数据
-		fmt.Sprintf("第%d轮猜测结果为:%s;结束时间为:%s", roundNumber, answer, RoundEndTime.Format("2006-01-02 15:04:05"))+fmt.Sprintf(";共耗时%s", RoundUseTimeStr))
+	round.Result = answer
+	round.UseTime = RoundUseTimeStr
+
 	var c string
-	fmt.Println("是否继续游玩?(Y/N)") //判断是否要继续游玩
+	fmt.Println("是否继续游玩?(Y/N)") // 判断是否要继续游玩
 	for {
 		fmt.Scan(&c)
 		if c == "Y" {
-			return true, roundData //继续游玩的话返回true
+			return true, round // 继续游玩的话返回 true
 		} else if c == "N" {
-			return false, roundData //不继续游玩的话返回false
+			return false, round // 不继续游玩的话返回 false
 		} else {
 			fmt.Println("输入错误请重新输入！")
 			continue
@@ -67,19 +83,35 @@ func guess(t int, roundNumber int) (bool, []string) {
 	}
 }
 
-// 打开game.txt并写入内容
-func writeFile(gameData []string) {
-	file, err := os.OpenFile("../game.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+// 打开 game.txt 并写入内容
+func writeFile(game Game) {
+	var temp int = 1
+	// 打开文件
+	file, err := os.OpenFile("../game.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("无法打开文件:%v", err)
 	}
 	defer file.Close()
 
-	for _, line := range gameData {
-		_, err = file.WriteString(line + "\n") // 将切片写入game.txt中
-		if err != nil {
-			log.Fatalf("无法写入文件:%v", err)
-		}
+	// 将 Game 结构体序列化为 JSON 格式
+	gameJson, err := json.Marshal(game)
+	if err != nil {
+		log.Fatalf("无法序列化数据:%v", err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		temp += 1
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("无法读取文件:%v", err)
+	}
+
+	data := fmt.Sprintf("第%v次游戏：%s\n", temp, string(gameJson))
+	// 写入文件
+	_, err = file.WriteString(data)
+	if err != nil {
+		log.Fatalf("无法写入文件:%v", err)
 	}
 }
 
@@ -97,7 +129,12 @@ func main() {
 	n := 0
 	gameStart := time.Now()
 	gameStartTime := gameStart.Format("2006-01-02 15:04:05")
-	gameData := []string{fmt.Sprintf("游戏开始时间：%s", gameStartTime)} //创建游戏总数据切片，并往整体游戏数据切片中添加游戏开始数据
+
+	game := Game{
+		GameCount: 0,
+		StartTime: gameStartTime,
+		Rounds:    []Round{},
+	}
 
 	for {
 		n++
@@ -105,7 +142,7 @@ func main() {
 		var num int
 		fmt.Scan(&num)
 		var t int
-		switch num { //选择难度
+		switch num { // 选择难度
 		case 1:
 			t = 3
 		case 2:
@@ -118,14 +155,15 @@ func main() {
 			continue
 		}
 
-		flag, roundData := guess(t, n)            //flag用于判断是否结束游戏，roundData是返回的该轮次数据切片
-		gameData = append(gameData, roundData...) //将返回的轮次数据切片保存到总数据切片
+		flag, round := guess(t, n)
+		game.GameCount++
+		game.Rounds = append(game.Rounds, round)
+
 		if !flag {
 			gameEnd := time.Now()
-			gameEndTime := gameEnd.Format("2006-01-02 15:04:05")
-			gameUseTime := gameEnd.Sub(gameStart)
-			gameData = append(gameData, fmt.Sprintf("游戏结束时间:%s,游戏总耗时:%s", gameEndTime, gameUseTime.String())) //将游戏结束时间保存进去
-			writeFile(gameData)
+			game.EndTime = gameEnd.Format("2006-01-02 15:04:05")
+			game.UseTime = gameEnd.Sub(gameStart).String()
+			writeFile(game)
 			return
 		}
 	}
